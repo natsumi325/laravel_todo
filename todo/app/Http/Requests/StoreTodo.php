e62d12b2c9f01a9665135d14c23fdbe9b120a602
natsumi325 <jiyama06@gmail.com>
2022-10-14 01:40:18 +0900

    ✨ フォームのバリデーション追加（laravel）

---
+++ todo/app/Http/Requests/StoreTodo.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
+
+class StoreTodo extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'task' => 'required',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'task.required' => 'タスクが未入力です',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(response()->json([
+            'message' => 'Failed validation',
+            'errors' => $errors,
+        ], 422, [], JSON_UNESCAPED_UNICODE));
+    }
+}
