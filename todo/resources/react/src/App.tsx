71e786be84e1d6c0290179d559218d099601f57b
natsumi325 <jiyama06@gmail.com>
2022-10-18 00:45:05 +0900

    🚧 レイアウト調整

--- todo/resources/react/src/App.tsx
+++ todo/resources/react/src/App.tsx
@@ -16,28 +16,30 @@ export const App = () => {
           <Button type="button">追加</Button>
         </Group>
       </Box>
-      <Tabs defaultValue="all">
-        <Tabs.List>
-          <Tabs.Tab value="all" icon={<IconBallpen size={24} />}>all</Tabs.Tab>
-          <Tabs.Tab value="incomplete" icon={<IconCircleDashed size={24} />}>incomplete</Tabs.Tab>
-          <Tabs.Tab value="complete" icon={<IconCircleCheck size={24} />}>complete</Tabs.Tab>
-        </Tabs.List>
+      <Box mt="30px">
+        <Tabs defaultValue="all" sx={{ maxWidth: "800px", margin: 'auto' }}>
+          <Tabs.List>
+            <Tabs.Tab sx={{ width: "33%" }} value="all" icon={<IconBallpen size={24} />}>all</Tabs.Tab>
+            <Tabs.Tab sx={{ width: "33%" }} value="incomplete" icon={<IconCircleDashed size={24} />}>incomplete</Tabs.Tab>
+            <Tabs.Tab sx={{ width: "33%" }} value="complete" icon={<IconCircleCheck size={24} />}>complete</Tabs.Tab>
+          </Tabs.List>
 
-        {/* 全todo一覧 */}
-        <Tabs.Panel value="all" pt="md">
-          all tab content
-        </Tabs.Panel>
+          {/* 全todo一覧 */}
+          <Tabs.Panel value="all" pt="md">
+            all tab content
+          </Tabs.Panel>
 
-        {/* 未todo一覧 */}
-        <Tabs.Panel value="incomplete" pt="md">
-          incomplete tab content
-        </Tabs.Panel>
+          {/* 未todo一覧 */}
+          <Tabs.Panel value="incomplete" pt="md">
+            incomplete tab content
+          </Tabs.Panel>
 
-        {/* 完了todo一覧 */}
-        <Tabs.Panel value="complete" pt="md">
-          complete tab content
-        </Tabs.Panel>
-      </Tabs>
+          {/* 完了todo一覧 */}
+          <Tabs.Panel value="complete" pt="md">
+            complete tab content
+          </Tabs.Panel>
+        </Tabs>
+      </Box>
     </Box>
   )
 };
