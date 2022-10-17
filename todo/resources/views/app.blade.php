09a93bcb0d5290cade4d88cb60f9ae871ca233f8
natsumi325 <jiyama06@gmail.com>
2022-10-18 00:45:55 +0900

    ⚡️ welcome.blade.phpファイル名変更

--- todo/resources/views/app.blade.php
+++ todo/resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravelaa</title>
+    <title>TODO APP</title>
     @viteReactRefresh
     @vite('resources/react/src/main.tsx')
 </head>
