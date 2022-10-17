a906ed9f29a2d0aca9fa8e352ee3736c6323c69e
natsumi325 <jiyama06@gmail.com>
2022-10-17 23:46:54 +0900

    ✨ react ルーティング設定　

--- todo/vite.config.ts
+++ todo/vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react'; // <- 追加
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            input: [
+                'resources/react/src/main.tsx'
+            ],
         }),
+        react(), // <- 追加
     ],
 });
